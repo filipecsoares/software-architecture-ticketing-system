d9a140947868bfbed1e3fac2d9b6b2da0ccdfdeb
Filipe Soares <filipecarlossoares@gmail.com>
2026-03-11 08:28:50 -0300

    init: create projects

---
+++ recommendation-service/src/test/java/com/fcs/core/recommendation/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcs.core.recommendation.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ review-service/src/test/java/com/fcs/core/review/review_service/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcs.core.review.review_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
