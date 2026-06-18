f5c9bd1437762b704b215e49c5713f3df44c090c
Filipe Soares <filipecarlossoares@gmail.com>
2026-06-18 08:15:48 -0300

    feat: Configure datasource and logging for admin service with H2 and Loki integration

--- services/admin-service/src/main/java/com/fcs/admin/AdminServiceApplication.java
+++ services/admin-service/src/main/java/com/fcs/admin/AdminServiceApplication.java
@@ -2,8 +2,12 @@ package com.fcs.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients
+@ComponentScan("com.fcs.admin")
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
